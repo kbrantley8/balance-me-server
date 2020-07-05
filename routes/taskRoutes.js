51216718d4f13688a22039dd496189e4525b772b
kbrantley <korybrantley@gmail.com>
2020-07-05 18:35:51 -0400

    asdf

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -341,11 +341,7 @@ router.get('/getTodaysTasks', async (req, res) => {
         // res.status(200).send(tasks)
         var start = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
         start = new Date(start);
-        start1 = new Date(start);
-        start.setHours(0,0,0,0);
-        start1.setHours(0, 0, 0, 0);
-        var start_tim = (start.getTime() / 1000)
-        res.status(200).send({"start": start_tim, "start1": start1.getTime() + (start1.getTimezoneOffset() * 60)})
+        res.status(200).send({"start": start})
 
     } catch (e) {
         return res.send({error: e.message})
