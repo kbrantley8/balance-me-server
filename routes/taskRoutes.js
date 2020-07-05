5e5d6df40223b8a908c2bc3cee547a9ead3b5830
kbrantley <korybrantley@gmail.com>
2020-07-05 18:24:52 -0400

    asdf

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -343,6 +343,7 @@ router.get('/getTodaysTasks', async (req, res) => {
         start = new Date(start);
         start1 = new Date(start);
         start.setHours(0,0,0,0);
+        start1.setHours(0, 0, 0, 0);
         var start_tim = (start.getTime() / 1000)
         res.status(200).send({"start": start_tim, "start1": start1})
 
